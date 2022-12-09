f061670954ac852efb9db55f55d4c35934f5314b
PiperChang <goddhqj8000@gmail.com>
2022-12-09 23:55:16 +0900

    Feat: header add - header css not finished

---
+++ components/layout/Header.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { Tabs, Tab } from "@mui/material";
+import { useRouter } from "next/router";
+
+const Header = () => {
+  const router = useRouter();
+  const [route, setRoute] = useState(router.route);
+
+  const handleChange = (e: React.SyntheticEvent, path: string) => {
+    router.push(path);
+    setRoute(path);
+  };
+
+  return (
+    <S.header>
+      <h1>
+        <img className="logo" src="/images/logo.png" alt="logo" />
+      </h1>
+      <nav>
+        <Tabs
+          value={route}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+        >
+          <Tab value="/post" label="Posts" />
+          <Tab value="/about" label="About" />
+        </Tabs>
+      </nav>
+    </S.header>
+  );
+};
+
+export default Header;
+
+const S = {
+  header: styled.header`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 64px;
+    border-bottom: 1px solid;
+    nav {
+      * {
+        margin: auto 0;
+        bottom: 0;
+      }
+      height: inherit;
+    }
+    .logo {
+      height: 64px;
+      object-fit: contain;
+    }
+  `,
+};
