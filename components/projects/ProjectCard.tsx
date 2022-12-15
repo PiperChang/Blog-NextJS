1d671b878931e226556309b711d31ebe3a5562b3
PiperChang <goddhqj8000@gmail.com>
2022-12-16 02:43:54 +0900

    style: Card style

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import React from "react";
-import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia,Divider,Paper } from "@mui/material";
 import { IProject } from "./project.d";
-import {AiFillGithub} from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
 import { SiNotion } from 'react-icons/si'
 import Typography from '@mui/material/Typography';
+
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)((props) => ({
+  // textAlign: 'center',
+  padding: 5,
+  color:  props.color,
+  marginRight: 2
+}));
 
 interface Props {
   data: IProject;
@@ -30,33 +40,41 @@ const ProjectCard = ({ data }: Props) => {
           />
         </div>
       </CardMedia>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent style={{padding: 10}}>
+        <Typography gutterBottom variant="h5" component="div" fontWeight="bold">
           {title}
         </Typography>
+        <Box height={45} overflow="hidden">
         <Typography variant="body2" component="div">
           <h3>{description}</h3>
         </Typography>
-        <Box>
-          {/* {tags.map((tag) => {
-            return (
-              <div>{tag}</div>)
-          })} */}
         </Box>
+        <Typography variant="body2" fontWeight="bold">
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{ height: 30 }}
+        >
+          {tags.map((tag) => {
+            return (
+              <Item key={tag.id} color={tag.color}>{tag.name}</Item>)
+          })}
+        </Stack>
+        </Typography>
+
       </CardContent>
-      <CardActions>
+      <CardActions style={{ padding: 10}}>
         <Typography gutterBottom variant="h6" component="div">
           <a href={githubLink}>
-            <Button size="small" color="primary">
-              <AiFillGithub size={25}/>
+            <Button size="small" color="primary" variant="outlined">
+              <AiFillGithub size={25} />
             </Button>
           </a>
           <a href={notionLink}>
-            <Button size="small" color="primary">
+            <Button size="small" color="primary" variant="outlined">
               <SiNotion size={25} />
             </Button>
           </a>
-
         </Typography>
       </CardActions>
     </Card>
@@ -64,3 +82,4 @@ const ProjectCard = ({ data }: Props) => {
 };
 
 export default ProjectCard;
+
