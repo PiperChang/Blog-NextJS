04b2b4f98cfa1c035f5e8b4fb7375ccfb8378308
KCHamp <goddhqj8000@ajou.ac.kr>
2022-12-18 17:54:24 +0900

    fix: fix page names, header link

--- components/works/ProjectCard.tsx
+++ components/works/ProjectCard.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import { IProject } from "./project.d";
-import { AiFillGithub } from 'react-icons/ai'
-import { SiNotion } from 'react-icons/si'
+import { IProject } from "./project";
+import { AiFillGithub } from "react-icons/ai";
+import { SiNotion } from "react-icons/si";
 
 interface Props {
   data: IProject;
@@ -12,53 +12,65 @@ const ProjectCard = ({ data }: Props) => {
   const title = data.properties.Name.title[0].plain_text;
   const subtitle = data.properties.Name.title[0].plain_text;
   const githubLink = data.properties.Github.url;
-  const notionLink = data.url
+  const notionLink = data.url;
   const description = data.properties.Description.rich_text[0].plain_text;
   const coverImg = data.cover.file?.url || data.cover.external.url;
   const tags = data.properties.Tags.multi_select;
 
   return (
-      <div className="project-card">
-        <div className="overflow-hidden relative h-28">
-          <Image
-            src={coverImg}
-            alt={`Cover Img ${title}`}
-            sizes="100vw"
-            fill
-            style={{ objectFit: 'cover', }}
-          />
-        </div>
-        <div className="p-3">
-          <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{subtitle}  </h2>
-          <h1 className="title-font text-lg mb-2 font-extrabold">{title}</h1> 
-          <div className="leading-relaxed mb-3 text-sm ">{description}</div>
-          <div className="flex items-center flex-wrap ">
-            {tags.map((tag) => {
-              return (
-                <div key={tag.id} className={`btn-tags bg-${tag.color}-400 dark:bg-${tag.color}-400 focus:outline-none`}>{tag.name}</div>
-              )
-            })}
-              <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-1 border-gray-400">
-                <a href={githubLink} className=" inline-flex items-center md:mb-2 lg:mb-0">
-                  <AiFillGithub size={25} />
-                  </a>
-                </span>
-              <span className="text-gray-500 inline-flex items-center leading-none text-sm">
-                <a href={notionLink} className=" inline-flex items-center md:mb-2 lg:mb-0">
-                  <SiNotion size={25} />
-                </a>
-              </span>
-          </div>
+    <div className="project-card">
+      <div className="overflow-hidden relative h-28">
+        <Image
+          src={coverImg}
+          alt={`Cover Img ${title}`}
+          sizes="100vw"
+          fill
+          style={{ objectFit: "cover" }}
+        />
       </div>
-  </div>
-
+      <div className="p-3">
+        <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+          {subtitle}{" "}
+        </h2>
+        <h1 className="title-font text-lg mb-2 font-extrabold">{title}</h1>
+        <div className="leading-relaxed mb-3 text-sm ">{description}</div>
+        <div className="flex items-center flex-wrap ">
+          {tags.map((tag) => {
+            return (
+              <div
+                key={tag.id}
+                className={`btn-tags bg-${tag.color}-400 dark:bg-${tag.color}-400 focus:outline-none`}
+              >
+                {tag.name}
+              </div>
+            );
+          })}
+          <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-1 border-gray-400">
+            <a
+              href={githubLink}
+              className=" inline-flex items-center md:mb-2 lg:mb-0"
+            >
+              <AiFillGithub size={25} />
+            </a>
+          </span>
+          <span className="text-gray-500 inline-flex items-center leading-none text-sm">
+            <a
+              href={notionLink}
+              className=" inline-flex items-center md:mb-2 lg:mb-0"
+            >
+              <SiNotion size={25} />
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default ProjectCard;
 
-
-{/* 
+{
+  /* 
 
       <Card key={data.id}>
         <CardMedia>
@@ -108,4 +120,5 @@ export default ProjectCard;
             </a>
           </Typography>
         </CardActions>
-      </Card> */}
+      </Card> */
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Hero from "components/home/Hero";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="description" content="만나서 반갑습니다. " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Hero />
     </div>
-  )
+  );
 }
