9a9efc55bfd94bf2c077146ca8b2c9115c96cea7
PiperChang <goddhqj8000@gmail.com>
2022-12-19 23:50:44 +0900

    WIP: Prisma schema 작성

---
+++ pages/projects/index.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import axios, { AxiosResponse } from 'axios';
+import { notionService } from '../../api';
+import { NOTION_DATABASE } from '../../constants/Notion';
+import { NOTION_TOKEN } from '../../constants/Notion';
+import { IProject } from './../../components/projects/project.d';
+import ProjectCard from 'components/projects/ProjectCard';
+interface Props {
+  projectData: Array<IProject>
+}
+
+const Projects = ({ projectData }: Props) => {
+  const projectList = projectData.map((project,idx) => {
+    return (
+        <ProjectCard key={`proj${idx}`} data={project} />
+    )
+  })
+
+  return (
+      <div className="container px-5 py-24 mx-auto">
+        <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {projectList}
+        </div>
+       </div>
+
+  )
+}
+
+export default Projects
+
+export async function getStaticProps() {
+  const options = {
+    method: 'POST',
+    url: `https://api.notion.com/v1/databases/${NOTION_DATABASE}/query`,
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${NOTION_TOKEN}`
+    },
+    data: { page_size: 100 }
+  };
+
+  const res = await axios.request(options)
+
+  const projectData = res.data.results
+
+  return {
+    props: { projectData }
+  }
+}
