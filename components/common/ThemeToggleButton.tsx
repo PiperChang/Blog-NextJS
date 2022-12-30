43b44fce1a8e6ed7af3391f7945ba2bab56471de
PiperChang <goddhqj8000@gmail.com>
2022-12-31 00:56:26 +0900

    fix: Hydration Error fix.
    - Dark mode 구현 시, Hydration이 생길 수 있음. Client에서 최초 설정된 모드와, server에서 렌더링 시 설정된 모드가 다를 수 있으니까.

--- components/common/ThemeToggleButton.tsx
+++ components/common/ThemeToggleButton.tsx
@@ -1,14 +1,19 @@
-import {useTheme} from 'next-themes'
-import {CiDark, CiLight} from "react-icons/ci"
+import { useTheme } from 'next-themes'
+import { CiDark, CiLight } from "react-icons/ci"
+import { useState, useEffect } from 'react';
 
 const ThemeToggleButton = () => {
-  const {theme, setTheme} = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   return (
-    <button className="inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:text-yellow-500 dark:hover:text-orange-500 text-base mt-4 md:mt-0" onClick={()=> setTheme(theme === 'dark' ? 'light' : 'dark')}>
-      { theme === 'dark' ? <CiLight strokeWidth={1}/> : <CiDark strokeWidth={1}/>}
+    <button className="inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:text-yellow-500 dark:hover:text-orange-500 text-base mt-4 md:mt-0" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {theme === 'dark' ? <CiLight strokeWidth={1} /> : <CiDark strokeWidth={1} />}
     </button>
-  ) 
+  )
 }
 
-export default ThemeToggleButton
+export default ThemeToggleButton  
